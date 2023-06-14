4bed18c8c4b6807ab7169988a3489d1a1c3d2a4a
Miko7845 <Assass1n.kuz@gmail.com>
2023-06-14 19:28:40 +0500

    Shooting with bullets

--- Assets/Scripts/WrappingEffect.cs
+++ Assets/Scripts/WrappingEffect.cs
@@ -1,18 +1,32 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class WrappingEffect : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    // Координата всех границ экрана.
+    private float screenTop = 10.0f;
+    private float screenBottom = -10.0f;
+    private float screenLeft = -18.0f;
+    private float screenRight = 18.0f;
 
-    // Update is called once per frame
     void Update()
     {
-        
+        ScreenWrap();
+    }
+
+    public void ScreenWrap()
+    {
+        Vector2 newPos = transform.position;                // Создаем вектор новой позиции и инициализируем его текущей позицией объекта
+
+        // Если объект вышел за границу экрана, перемещаем его на противоположную границу экрана.
+        if (transform.position.y > screenTop)
+            newPos.y = screenBottom;
+        else if (transform.position.y < screenBottom)
+            newPos.y = screenTop;
+        else if (transform.position.x < screenLeft)
+            newPos.x = screenRight;
+        else if (transform.position.x > screenRight)
+            newPos.x = screenLeft;
+
+        transform.position = newPos;                        // Обновляем позицию объекта с новым вектором
     }
 }
